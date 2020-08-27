aa1d04df3f44d20c83e614db33a6e5a83df2fa39
Marcus Damgaard Bournonwille <marc8847@zbc.dk>
2020-08-28 01:07:54 +0200

    Dictionary?

---
+++ Jedi_Mind_Tricks/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jedi_Mind_Tricks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> people = new Dictionary<string, int>();
+            people.Add("Marcus", 18);
+            people["Sebastian"] = new people();
+        }
+    }
+}
